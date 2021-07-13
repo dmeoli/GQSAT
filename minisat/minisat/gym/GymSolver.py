29d84e9acdd06b9c14da12fca2d327ceb61c5e8a
Donato Meoli <donato.meoli.95@gmail.com>
2021-07-13 14:28:06 +0200

    fixed .gitignore

--- minisat/minisat/gym/GymSolver.py
+++ minisat/minisat/gym/GymSolver.py
@@ -5,45 +5,33 @@
 # the SWIG interface file instead.
 
 from sys import version_info as _swig_python_version_info
-
 if _swig_python_version_info >= (2, 7, 0):
-
     def swig_import_helper():
         import importlib
-
-        pkg = __name__.rpartition(".")[0]
-        mname = ".".join((pkg, "_GymSolver")).lstrip(".")
+        pkg = __name__.rpartition('.')[0]
+        mname = '.'.join((pkg, '_GymSolver')).lstrip('.')
         try:
             return importlib.import_module(mname)
         except ImportError:
-            return importlib.import_module("_GymSolver")
-
-
+            return importlib.import_module('_GymSolver')
     _GymSolver = swig_import_helper()
     del swig_import_helper
 elif _swig_python_version_info >= (2, 6, 0):
-
     def swig_import_helper():
         from os.path import dirname
         import imp
-
         fp = None
         try:
-            fp, pathname, description = imp.find_module(
-                "_GymSolver", [dirname(__file__)]
-            )
+            fp, pathname, description = imp.find_module('_GymSolver', [dirname(__file__)])
         except ImportError:
             import _GymSolver
-
             return _GymSolver
         try:
-            _mod = imp.load_module("_GymSolver", fp, pathname, description)
+            _mod = imp.load_module('_GymSolver', fp, pathname, description)
         finally:
             if fp is not None:
                 fp.close()
         return _mod
-
-
     _GymSolver = swig_import_helper()
     del swig_import_helper
 else:
@@ -60,18 +48,17 @@ try:
 except ImportError:
     import __builtin__
 
-
 def _swig_setattr_nondynamic(self, class_type, name, value, static=1):
-    if name == "thisown":
+    if (name == "thisown"):
         return self.this.own(value)
-    if name == "this":
-        if type(value).__name__ == "SwigPyObject":
+    if (name == "this"):
+        if type(value).__name__ == 'SwigPyObject':
             self.__dict__[name] = value
             return
     method = class_type.__swig_setmethods__.get(name, None)
     if method:
         return method(self, value)
-    if not static:
+    if (not static):
         if _newclass:
             object.__setattr__(self, name, value)
         else:
@@ -85,14 +72,12 @@ def _swig_setattr(self, class_type, name, value):
 
 
 def _swig_getattr(self, class_type, name):
-    if name == "thisown":
+    if (name == "thisown"):
         return self.this.own()
     method = class_type.__swig_getmethods__.get(name, None)
     if method:
         return method(self)
-    raise AttributeError(
-        "'%s' object has no attribute '%s'" % (class_type.__name__, name)
-    )
+    raise AttributeError("'%s' object has no attribute '%s'" % (class_type.__name__, name))
 
 
 def _swig_repr(self):
@@ -100,24 +85,15 @@ def _swig_repr(self):
         strthis = "proxy of " + self.this.__repr__()
     except __builtin__.Exception:
         strthis = ""
-    return "<%s.%s; %s >" % (
-        self.__class__.__module__,
-        self.__class__.__name__,
-        strthis,
-    )
-
+    return "<%s.%s; %s >" % (self.__class__.__module__, self.__class__.__name__, strthis,)
 
 try:
     _object = object
     _newclass = 1
 except __builtin__.Exception:
-
     class _object:
         pass
-
-
     _newclass = 0
-
 
 class GymSolver(_object):
     __swig_setmethods__ = {}
@@ -153,12 +129,11 @@ class GymSolver(_object):
 
     def getClauses(self):
         return _GymSolver.GymSolver_getClauses(self)
-
     __swig_destroy__ = _GymSolver.delete_GymSolver
     __del__ = lambda self: None
-
-
 GymSolver_swigregister = _GymSolver.GymSolver_swigregister
 GymSolver_swigregister(GymSolver)
 
 # This file is compatible with both classic and new-style classes.
+
+
